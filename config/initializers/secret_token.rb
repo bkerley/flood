19fac98f71273de02bbe355d093d84fa53dba327
Bryce Kerley <bkerley@brycekerley.net>
2013-06-01 15:09:28 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flood::Application.config.secret_key_base = '0e34840a8e32458dc2a908d2f76c57fdb013ee224aa66743a9637a3413d6923b6dbce981e34fe5f0ac22e9d5d9486f24fe44078e7f9465c0f144616d32c7f946'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flood::Application.config.session_store :cookie_store, key: '_flood_session'
